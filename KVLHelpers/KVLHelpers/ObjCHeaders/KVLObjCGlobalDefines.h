254a912a591905e55a22b54d98e786661c65b6b2
Misha <mikoval@gmail.com>
2015-09-27 15:33:19 +0300

    Small correction

--- KVLHelpers/KVLHelpers/ObjCHeaders/KVLObjCGlobalDefines.h
+++ KVLHelpers/KVLHelpers/ObjCHeaders/KVLObjCGlobalDefines.h
@@ -9,7 +9,7 @@
 #ifndef KVLGlobalDefines_h
 #define KVLGlobalDefines_h
 
-#define GetCodeLocation [NSString stringWithFormat:@"\n➕%s:%d",__PRETTY_FUNCTION__,__LINE__]
+#define GetCodeLocation [NSString stringWithFormat:@"%s:%d",__PRETTY_FUNCTION__,__LINE__]
 
 #define RandomColor [UIColor RGBA:(random() % 255) g:(random() % 255) b:(random() % 255) a: 0.7]
 #define BlackColor [UIColor blackColor]
