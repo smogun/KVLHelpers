5149c4fb75a2ef2809bba45322d26be056b2f266
Misha <mikoval@gmail.com>
2015-09-29 10:32:53 +0300

    Extended UIView extension

--- KVLHelpers/KVLHelpers/Extensions/UIView+Extension.swift
+++ KVLHelpers/KVLHelpers/Extensions/UIView+Extension.swift
@@ -13,6 +13,34 @@ public extension UIView
     public var origin:CGPoint{get{return self.frame.origin}}
     public var size:CGSize{get{return self.frame.size}}
     
-    public var endX:CGFloat{get {return self.frame.origin.x + self.frame.size.width}}
+    public var endX:CGFloat{get{return self.frame.origin.x + self.frame.size.width}}
     public var endY:CGFloat{get{return self.frame.origin.y + self.frame.size.height}}
+    
+    public func addSubviewAtCenter(subView: UIView?)
+    {
+        if (subView == nil)
+        {
+            return;
+        }
+        
+        var subViewFrame = subView!.frame;
+        subViewFrame.origin.x = self.size.width / 2 - subViewFrame.size.width / 2;
+        subViewFrame.origin.y = self.size.height / 2 - subViewFrame.size.height / 2;
+        subView!.frame = subViewFrame;
+        self.addSubview(subView!);
+    }
+    
+    public func addSubviewAtCenterHorizontally(subView: UIView?, originY: CGFloat?)
+    {
+        if (subView == nil)
+        {
+            return;
+        }
+        
+        var subViewFrame = subView!.frame;
+        subViewFrame.origin.x = self.size.width / 2 - subViewFrame.size.width / 2;
+        subViewFrame.origin.y = originY == nil ? 0.0 : originY!;
+        subView!.frame = subViewFrame;
+        self.addSubview(subView!);
+    }
 }
